8fda8d26c6ed78796e2ef53a8a97e028e82b1ea5
-v <gustavoleck@hotmail.com>
2023-02-22 22:13:59 -0300

    Criar estrura de pastas para utilizar clean architecture

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express  from "express";
+import status from "./routes/healthRoute.js"
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+console.clear();
+app.listen(3000, () => {
+    console.log("-- Server ON --")
+    app.use('/api',status)
+});
