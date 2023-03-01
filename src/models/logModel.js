dc9855273d5347f6e3a22a7c3e9d838d4d34aaf3
-v <gustavoleck@hotmail.com>
2023-02-28 23:52:01 -0300

    Fix(Alterado nome de parametro e adicionado atributa 'Status')

--- src/models/logModel.js
+++ src/models/logModel.js
@@ -1,9 +1,10 @@
 import moment from "moment";
 
-export class LogModel{
-    constructor(erro){
-        this.operation = erro.operation;
-        this.message = erro.message;
-        this.data = moment().format(); 
-    }
+export class LogModel {
+  constructor(log) {
+    this.status = log.status;
+    this.operation = log.operation;
+    this.message = log.message;
+    this.data = moment().format();
+  }
 }
