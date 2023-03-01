9174ed6b457b2ad3e761153e57107203b1b876b5
-v <gustavoleck@hotmail.com>
2023-02-28 21:36:01 -0300

    Build(Chamada de rota em index do projeto)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import express  from "express";
+import express from "express";
 import status from "./routes/statusRoute.js";
+import cratePresenca from "./routes/Create-Presenca-Route.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 console.clear();
 app.listen(3000, () => {
-    console.log("-- Server ON --")
-    app.use('/api', status)
+  console.log("-- Server ON --");
+  app.use("/api", status);
+  app.use("/api", cratePresenca);
 });
