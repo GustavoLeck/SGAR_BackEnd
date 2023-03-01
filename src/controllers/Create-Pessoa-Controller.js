7773ab0357201ac32dc8f6dfa87e95911d674f29
-v <gustavoleck@hotmail.com>
2023-02-28 22:03:11 -0300

    (Criado view para cadastrar presenca)

--- src/controllers/Create-Pessoa-Controller.js
+++ src/controllers/Create-Pessoa-Controller.js
@@ -1,8 +1,9 @@
+import moment from "moment";
 import { CreatePresencaUseCase } from "../useCases/Create-Presenca-UseCase.js";
 
 export class CreatePresencaController {
   async handle(req, res) {
     const criarPresenca = await new CreatePresencaUseCase().execute(req.body);
-    res.status(200).send({ message: "Sucess" });
+    res.status(200).send({ Status: "Sucess", Message: criarPresenca });
   }
 }
