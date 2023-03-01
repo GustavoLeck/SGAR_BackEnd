77ecbbffe1c34d02eaee477aec5d4c02a6ccdd69
-v <gustavoleck@hotmail.com>
2023-03-01 00:16:27 -0300

    Finalizado Controller de Rota de Criacao de Presenca

--- src/controllers/Create-Pessoa-Controller.js
+++ src/controllers/Create-Pessoa-Controller.js
@@ -4,6 +4,9 @@ import { CreatePresencaUseCase } from "../useCases/Create-Presenca-UseCase.js";
 export class CreatePresencaController {
   async handle(req, res) {
     const criarPresenca = await new CreatePresencaUseCase().execute(req.body);
-    res.status(200).send({ Status: "Sucess", Message: criarPresenca });
+    if (criarPresenca.status != "Sucesso") {
+      res.status(500).send(criarPresenca);
+    }
+    res.status(200).send(criarPresenca);
   }
 }
