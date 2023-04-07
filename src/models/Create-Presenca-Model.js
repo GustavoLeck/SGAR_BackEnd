a7933c5a9a43f4ca8c22deee093739c0716c3282
Gustavo_Leck <gustavoleck@hotmail.com>
2023-04-07 12:22:20 -0300

    build(Alter tipo de data): Alterado tipo de data para local;
    - Alterado modo de formação de data em model para dia, mes, ano;

--- src/models/Create-Presenca-Model.js
+++ src/models/Create-Presenca-Model.js
@@ -2,7 +2,7 @@ import moment from "moment/moment.js";
 export class CreatePresencaModel {
   constructor(presenca) {
     this.TempoAtivo = presenca.TempoAtivo;
-    this.Data = moment().toDate();
+    this.Data = moment().locale("pt-br").format("DD-mm-YYYY");
     this.Usuario = presenca.Usuario;
     this.Organizacao = presenca.Organizacao;
     this.QuantidadeDeMovimentos = presenca.QuantidadeDeMovimentos;
